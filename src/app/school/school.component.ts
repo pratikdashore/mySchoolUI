cb0b95dfab3511e75ce4df9baa5aa145cf5b63e8
Pratik Dashore <pratikdashore@hotmail.com>
2017-03-12 18:03:29 +0530

    added first service

--- src/app/school/school.component.ts
+++ src/app/school/school.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SchoolInfoService} from './school-info.service';
 
 @Component({
   selector: 'app-school',
   templateUrl: './school.component.html'
 })
 export class SchoolComponent implements OnInit {
-
-  name : String = 'School Component';
-  constructor() { }
+  
+  private schoolInfoService : SchoolInfoService;
+  name : String = 'School Component ';
+  constructor(SchoolInfoService : SchoolInfoService) { 
+    this.schoolInfoService = SchoolInfoService;
+  }
 
   ngOnInit() {
-  }
+   console.log(this.schoolInfoService);  
+}
 
 }
--- src/app/school/school.module.ts
+++ src/app/school/school.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SchoolInfoModule } from './school-info/school-info.module';
 import { SchoolComponent } from './school.component';
+import { SchoolInfoService } from './school-info.service';
+
 @NgModule({
   imports: [
     CommonModule,
     SchoolInfoModule
   ],
   declarations: [SchoolComponent],
+  providers:[SchoolInfoService],
   exports:[SchoolComponent],
   bootstrap: [SchoolComponent]
 })
