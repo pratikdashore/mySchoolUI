f94e6513c2345b45b1836e3e0b3201096fdb11e7
Pratik Dashore <pratikdashore@hotmail.com>
2017-04-04 09:30:18 +0530

    basic form included with material

--- src/app/school/school.module.ts
+++ src/app/school/school.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { SchoolComponent } from './school.component';
 import { SchoolRoutingModule, routedComponents } from './school-router/school-router.module';
 import { SchoolSearchComponent } from './school-search/school-search.component';
 
 @NgModule({
   imports: [
-    CommonModule, SchoolRoutingModule
+    CommonModule, SchoolRoutingModule, MaterialModule, FormsModule, HttpModule
   ],
   declarations: [routedComponents, SchoolSearchComponent],
   providers: [],
