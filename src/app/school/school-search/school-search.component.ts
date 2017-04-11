5da12889b9fc08de65bdb9f62912a8ce7fe7a2f5
Pratik Dashore <pratikdashore@hotmail.com>
2017-04-11 15:29:58 +0530

    changes for events

--- src/app/school/school-search/school-search.component.ts
+++ src/app/school/school-search/school-search.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SchoolSearchComponent implements OnInit {
   name: string;
+  lName: string;
+  onClick(): void {
+    this.lName = this.name;
+    alert(this.lName);
+  };
   constructor() { }
 
   ngOnInit() {
